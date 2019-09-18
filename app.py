3813a946ccd5a62740c635da97e4e7209386a13f
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-09-18 21:33:53 +0800

    Testing interaction end point

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def removeUserAction(slack_request):
 @app.route('/interaction', methods=['POST'])
 def interactionTest():
     slack_request = request
-    print(slack_request)
+    print(slack_request.get_data())
     return 'test'
 
 if __name__ == '__main__':
