146f3936d305010aee80d832fe8e4b2eb26d02a2
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-09-18 22:35:13 +0800

    Testing interaction end point

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import re
 import os
 import sys
-import ast
+import json
 import time
 import slack
 import requests
@@ -191,7 +191,7 @@ def interactionTest():
     return 'test'
 
 def removeUserAction2(slack_request):
-    payload = ast.literal_eval(slack_request['payload'])
+    payload = json.loads(slack_request['payload'])
     print(payload)
     responseUrl = payload['response_url']
     actionValue = payload['actions']['value']
